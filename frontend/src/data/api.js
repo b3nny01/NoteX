dc7aae10dc1520b3cd38b9cd609a81225046f02f
ALESSIO BENENATI <benenati2001@gmail.com>
2024-09-17 15:02:44 +0200

    started fixing vurtual desk, TODO: fix newNote and updateNote modals

--- frontend/src/data/api.js
+++ frontend/src/data/api.js
@@ -3,30 +3,30 @@ import API from "../utils/API";
 const APIs = {
 
     // registration features
-    registration:       new API("http://192.168.1.2:8080/notex_backend/registrationServlet","registration","POST"),
+    registration:       new API("http://localhost:8080/notex_backend/registrationServlet","registration","POST"),
 
     // login features
-    login:              new API("http://192.168.1.2:8080/notex_backend/loginAndAuthServlet","signedUserLogin","POST"),
-    logout:             new API("http://192.168.1.2:8080/notex_backend/loginAndAuthServlet","logout","POST"),  
-    retrieveSession:    new API("http://192.168.1.2:8080/notex_backend/loginAndAuthServlet","getSession","POST"),
+    login:              new API("http://localhost:8080/notex_backend/loginAndAuthServlet","signedUserLogin","POST"),
+    logout:             new API("http://localhost:8080/notex_backend/loginAndAuthServlet","logout","POST"),  
+    retrieveSession:    new API("http://localhost:8080/notex_backend/loginAndAuthServlet","getSession","POST"),
 
     // User features
-    search:             new API("http://192.168.1.2:8080/notex_backend/searchServlet","search","GET"),
+    search:             new API("http://localhost:8080/notex_backend/searchServlet","search","GET"),
 
     // SignedUser features
-    createNote:         new API("http://192.168.1.2:8080/notex_backend/signedUserServlet","createNote","POST"),
-    retrieveNotes:      new API("http://192.168.1.2:8080/notex_backend/signedUserServlet","retrieveNotes","GET"),
-    updateNote:         new API("http://192.168.1.2:8080/notex_backend/signedUserServlet","updateNote","PUT"),
-    deleteNote:         new API("http://192.168.1.2:8080/notex_backend/signedUserServlet","deleteNote","DELETE"),
+    createNote:         new API("http://localhost:8080/notex_backend/signedUserServlet","createNote","POST"),
+    retrieveNotes:      new API("http://localhost:8080/notex_backend/signedUserServlet","retrieveNotes","GET"),
+    updateNote:         new API("http://localhost:8080/notex_backend/signedUserServlet","updateNote","PUT"),
+    deleteNote:         new API("http://localhost:8080/notex_backend/signedUserServlet","deleteNote","DELETE"),
 
-    createNotebook:     new API("http://192.168.1.2:8080/notex_backend/signedUserServlet","createNotebook","POST"),
-    retrieveNotebooks:  new API("http://192.168.1.2:8080/notex_backend/signedUserServlet","retrieveNotebooks","GET"),
-    updateNotebook:     new API("http://192.168.1.2:8080/notex_backend/signedUserServlet","updateNotebook","PUT"),
-    deleteNotebook:     new API("http://192.168.1.2:8080/notex_backend/signedUserServlet","deleteNotebook","DELETE"),
+    createNotebook:     new API("http://localhost:8080/notex_backend/signedUserServlet","createNotebook","POST"),
+    retrieveNotebooks:  new API("http://localhost:8080/notex_backend/signedUserServlet","retrieveNotebooks","GET"),
+    updateNotebook:     new API("http://localhost:8080/notex_backend/signedUserServlet","updateNotebook","PUT"),
+    deleteNotebook:     new API("http://localhost:8080/notex_backend/signedUserServlet","deleteNotebook","DELETE"),
 
     // Review features
-    sendReview:         new API("http://192.168.1.2:8080/notex_backend/reviewServlet","sendReview","POST"),
-    retrieveReviews:    new API("http://192.168.1.2:8080/notex_backend/reviewServlet","retrieveReviews","GET")
+    sendReview:         new API("http://localhost:8080/notex_backend/reviewServlet","sendReview","POST"),
+    retrieveReviews:    new API("http://localhost:8080/notex_backend/reviewServlet","retrieveReviews","GET")
 };
 
 export default APIs
